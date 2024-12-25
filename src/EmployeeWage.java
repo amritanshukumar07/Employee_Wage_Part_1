0d6ffc709fe055cdeca76e64cd085d020a0b775c
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-25 20:17:29 +0530

    Company Wage Builder System added

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,47 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 public class EmployeeWage {
-//    private static final int WAGE_PER_HOUR = 20;
-//    private static final int FULL_DAY_HOUR = 8;
-//    private static final int PART_TIME_HOUR = 4;
-//    private static final int MAX_WORKING_DAYS = 20;
-//    private static final int MAX_WORKING_HOURS = 100;
-    public static Map<String,Integer> companyWage = new HashMap<>();
-    public static void EmployeeWageCalculator( String companyName, int WAGE_PER_HOUR,int MAX_WORKING_DAYS,int MAX_WORKING_HOURS,int FULL_DAY_HOUR,int PART_TIME_HOUR){
-        int totalHours=0;
-        int totalDays=0;
-        int totalWage=0;
 
-        Random random= new Random();
-        while (totalHours < MAX_WORKING_HOURS && totalDays < MAX_WORKING_DAYS) {
-            int workHours = getWorkHours(random.nextInt(3),FULL_DAY_HOUR,PART_TIME_HOUR);
-            totalHours += workHours;
-            totalDays++;
-            totalWage += workHours * WAGE_PER_HOUR;
-
-        }
-        System.out.println("Employee Wage Calculation for Company: "+companyName);
-        System.out.println("Total Days Worked: " + totalDays);
-        System.out.println("Total Hours Worked: " + totalHours);
-        System.out.println("Total Monthly Wage: " + totalWage);
-        companyWage.put(companyName,totalWage);
-    }
-    public static int getWorkHours(int empType,int FULL_DAY_HOUR,int PART_TIME_HOUR){
-        return switch (empType) {
-            case 1 -> FULL_DAY_HOUR;
-            case 2 -> PART_TIME_HOUR;
-            default -> 0;
-        };
-    }
-    public static void displayAll(){
-        System.out.println("Company Wages : "+companyWage);
-    }
     public static void main(String [] args){
-        System.out.println("Welcome to Employee wage computation for different companies");
-        EmployeeWageCalculator("GE Vernova",20,20,100,8,4);
-        EmployeeWageCalculator("GE Health",30,20,100,8,4);
-        displayAll();
+        System.out.println("Welcome to Employee wage builder for different companies");
+        EmployeeWageBuilder builder= new EmployeeWageBuilder();
+        builder.addCompany("GE Vernova",20,20,100,8,4);
+        builder.addCompany("GE Health",30,20,100,8,4);
+        builder.calculateWages();
+        for(Company company: builder.companies){
+            System.out.println("Total Wage of Company "+company.name+" : " + company.totalWage);
+        }
     }
 }
---
+++ src/EmployeeWageBuilder.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class EmployeeWageBuilder {
+    public List<Company>companies;
+
+    public EmployeeWageBuilder(){
+        companies= new ArrayList<>();
+
+    }
+
+    public void addCompany(String name,int wagePerHour, int fullDayHour, int partTimeHour, int maxWorkingDays, int maxWorkingHours){
+        companies.add(new Company(name, wagePerHour, fullDayHour, partTimeHour, maxWorkingDays, maxWorkingHours));
+    }
+
+    public void calculateWages(){
+        for( Company company:companies){
+            int totalWage= calculateWageForCompany(company);
+            company.totalWage=totalWage;
+        }
+    }
+    private int calculateWageForCompany(Company company){
+        int totalHours=0;
+        int totalDays=0;
+        int totalWage=0;
+
+        Random random= new Random();
+        while (totalHours < company.maxWorkingHours && totalDays < company.maxWorkingDays) {
+            int workHours = getWorkHours(random.nextInt(3),company.fullDayHour,company.partTimeHour);
+            totalHours += workHours;
+            totalDays++;
+            totalWage += workHours * company.wagePerHour;
+
+        }
+        return totalWage;
+    }
+    public static int getWorkHours(int empType,int fullDayHour,int partTimeHour){
+        return switch (empType) {
+            case 1 -> fullDayHour;
+            case 2 -> partTimeHour;
+            default -> 0;
+        };
+    }
+}
